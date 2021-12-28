f16450a13137eaabbea4c8feffbb9893f8ab0223
André Lacerda <andrlacerda180@gmail.com>
2021-12-28 10:33:23 -0300

    add UsersListComponent

---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,12 @@
+import { Card } from '../UI/Card'
+import styles from './UsersList.module.css'
+
+export function UsersList(props){
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map(user => <li key={user.id}>{user.name} ({user.age} years old)</li>)}
+      </ul>
+    </Card>
+  )
+}
